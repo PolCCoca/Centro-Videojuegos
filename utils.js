8f37a06e36b9987c30cbef977870464b7d8cf1b7
Pol Codina <156607117+PolCCoca@users.noreply.github.com>
2025-06-06 11:47:52 +0200

    Delete utils.js

--- utils.js
+++
@@ -1,9 +0,0 @@
-function getUsers() {
-  return JSON.parse(localStorage.getItem('usuaris')) || {};
-}
-function saveUsers(users) {
-  localStorage.setItem('usuaris', JSON.stringify(users));
-}
-function getUsuariActiu() {
-  return localStorage.getItem('usuariActiu');
-}
